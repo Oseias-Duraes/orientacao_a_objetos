332017584554d4e2851a3a827b6489ed48d790f8
Oséias Alves Durães <oseias.duraes@outlook.com>
2022-06-07 19:27:27 -0300

    Versão 07-06 correta

--- carro.py
+++ carro.py
@@ -1,13 +1,13 @@
 class Carro(): #classe é um carro por exemplo
     """Essa é a classe carro. Esta classe é utilizada para instanciar novos carros em nosso programa"""
-    def __init__(self, cor, qtd_portas, tipo_combustivel, potencia, qtd_combustivel,is_ligado,velocidade):# atributos são as caracteristicas
+    def __init__(self, cor, qtd_portas, tipo_combustivel, potencia):# atributos são as caracteristicas
         self.cor = cor
         self.qtd_portas = qtd_portas
         self.combustivel = tipo_combustivel
         self.potencia = potencia
-        self.qtd_combustivel = qtd_combustivel
-        self.is_ligado = is_ligado
-        self.velocidade = velocidade
+        self.qtd_combustivel = 0
+        self.is_ligado = False
+        self.velocidade = 0
 
     def abastecer (self, qtd_combustivel): #def sempre cria função. Nesse caso estamos criando um método, método abastecer
         """O método abastecer, recebe como parâmetro a quantidade de combustivel e coloca no atributo qtd_combustivel o objeto carro"""
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import carro
 
-uno_vermelho = carro.Carro("vermelho", 4, "Flex", 1.0, 0, False, 0) #isso é um objeto, é a declaração do que é o carro#
+uno_vermelho = carro.Carro("vermelho", 4, "Flex", 1.0) #isso é um objeto, é a declaração do que é o carro#
 #help(carro.Carro)
 uno_vermelho.ligar() #chamando o método ligar da classe uno para o carro uno vermelho que contem as propriedades acima
 uno_vermelho.abastecer(50)
 uno_vermelho.abastecer(10)
 
-qtd_abastecida = uno_vermelho.abastecer(input(f"Quantos litros gostaria de abastecer"))
+#qtd_abastecida = uno_vermelho.abastecer(input(f"Quantos litros gostaria de abastecer"))
 print(f"A quantidade de combustível do carro é: {uno_vermelho.qtd_combustivel}")
 
-uno_preto = carro.Carro("preto", 2, "Flex", 1.4, 0, False, 0)
+uno_preto = carro.Carro("preto", 2, "Flex", 1.4)
 uno_preto.desligar()
 print(f"A quantidade de combustível do carro é: {uno_preto.qtd_combustivel}")
 uno_preto.ligar()
